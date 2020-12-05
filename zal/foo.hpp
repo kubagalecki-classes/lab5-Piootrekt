02fc9e5c4944710e6fca439c160b70d0a8c82992
Piootrekt <ppreform@gmail.com>
2020-12-05 22:23:03 +0000

    proba2

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -8,29 +8,26 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
+    std::list<char> s1;
     std::vector< char > w1;
-    char add;
-    /*for (Human p : people)
-    {
-        p.birthday();
-    }*/
+    char add = 0;
     for (auto it = people.rbegin(); it != people.rend(); ++it)
     {
         it->birthday();
         switch (it->isMonster())
         {
-          case false:
-          {
+        case false:
+        {
             add = 'y';
             break;
-          }
-          case true:
-          {
+        }
+        case true:
+        {
             add = 'n';
             break;
-          }
-        w1.push_back(add);
         }
+        }
+        w1.push_back(add);
     }
     return w1;
 }
